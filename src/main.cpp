20163aa23c6e2ec2fae1d03723e9f3d207e4b6b5
Oliver Ellis <ellis.oliver@gmail.com>
2025-03-29 10:45:22 +0100

    Added three column layout

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
 #include <QApplication>
-#include <QMainWindow>
+#include <QWidget>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLabel>
 #include <QPushButton>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    QMainWindow mainWindow;
-    mainWindow.setWindowTitle("My Qt Application");
+    QWidget window;
+    window.setWindowTitle("LLM Manager");  // Set the window title
+    window.resize(800, 600);  // Set the initial window size to 800x600 pixels
 
-    QPushButton button("Click Me", &mainWindow);
-    button.setGeometry(100, 100, 100, 30);
+    QHBoxLayout *mainLayout = new QHBoxLayout;
 
-    mainWindow.show();
+    // Left column layout
+    QWidget *leftColumn = new QWidget;
+    QVBoxLayout *leftLayout = new QVBoxLayout(leftColumn);
+    leftLayout->addWidget(new QLabel("Left Column Widget 1"));
+    leftColumn->setFixedWidth(150);  // Set fixed width for left column
+
+    // Middle column layout
+    QVBoxLayout *middleLayout = new QVBoxLayout;
+    middleLayout->addWidget(new QLabel("Middle Column Widget 1"));
+
+    // Right column layout
+    QWidget *rightColumn = new QWidget;
+    QVBoxLayout *rightLayout = new QVBoxLayout(rightColumn);
+    rightLayout->addWidget(new QLabel("Right Column Widget 1"));
+    rightColumn->setFixedWidth(150);  // Set fixed width for right column
+
+    // Add column layouts to the main layout
+    mainLayout->addWidget(leftColumn);  // Fixed width column
+    mainLayout->addLayout(middleLayout); // Flexible width column
+    mainLayout->addWidget(rightColumn);  // Fixed width column
+
+    window.setLayout(mainLayout);
+    window.show();
 
     return app.exec();
 }
