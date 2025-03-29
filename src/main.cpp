90d9290f0a374012da304ba4eb949d97deb1c1f7
Oliver Ellis <ellis.oliver@gmail.com>
2025-03-29 10:23:19 +0100

    Added initial QT code to program

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
+#include <QApplication>
+#include <QMainWindow>
+#include <QPushButton>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    QApplication app(argc, argv);
+
+    QMainWindow mainWindow;
+    mainWindow.setWindowTitle("My Qt Application");
+
+    QPushButton button("Click Me", &mainWindow);
+    button.setGeometry(100, 100, 100, 30);
+
+    mainWindow.show();
+
+    return app.exec();
 }
