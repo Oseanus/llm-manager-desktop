de2fa08ba91b2a48e9eb2579580f60c63bc26915
Oliver Ellis <ellis.oliver@gmail.com>
2025-04-17 23:44:10 +0200

    Relocated setupd to MainWidget

--- src/MainWidget.cpp
+++ src/MainWidget.cpp
@@ -3,7 +3,43 @@
 MainWidget::MainWidget(QWidget *parent)
     : QWidget(parent)
 {
+    _mainLayout = new QHBoxLayout(this);
+
+    // Left column layout
+    _leftColumn = new QWidget();
+    _leftLayout = new QVBoxLayout(_leftColumn);
     
+    _llmMenu = new QComboBox();
+    _llmMenu->addItem("Item 1");
+    _llmMenu->addItem("Item 2");
+    _llmMenu->addItem("Item 3");
+    _leftLayout->addWidget(_llmMenu);
+    
+    _leftLabel = new QLabel("Left Column Widget 1");
+    _leftLayout->addWidget(_leftLabel);
+    _leftColumn->setFixedWidth(150);  // Set fixed width for left column
+
+    // Middle column layout
+    _middleColumn = new QWidget();
+    _middleLayout = new QVBoxLayout(_middleColumn);
+
+    _middleLabel = new QLabel("Middle Column Widget 1");
+    _middleLayout->addWidget(_middleLabel);
+
+    // Right column layout
+    _rightColumn = new QWidget();
+    _rightLayout = new QVBoxLayout(_rightColumn);
+
+    _rightLabel = new QLabel("Right Column Widget 1");
+    _rightLayout->addWidget(_rightLabel);
+    _rightColumn->setFixedWidth(150);  // Set fixed width for right column
+
+    // Add column layouts to the main layout
+    _mainLayout->addWidget(_leftColumn);  // Fixed width column
+    _mainLayout->addWidget(_middleColumn); // Flexible width column
+    _mainLayout->addWidget(_rightColumn);  // Fixed width column
+
+    setLayout(_mainLayout);
 }
 
 MainWidget::~MainWidget()
--- src/MainWidget.h
+++ src/MainWidget.h
@@ -6,7 +6,18 @@
 class MainWidget : public QWidget
 {
 private:
-    /* data */
+    QPointer<QHBoxLayout> _mainLayout;
+    QPointer<QWidget> _leftColumn;
+    QPointer<QVBoxLayout> _leftLayout;
+    QPointer<QComboBox> _llmMenu;
+    QPointer<QLabel> _leftLabel;
+    QPointer<QWidget> _middleColumn;
+    QPointer<QVBoxLayout> _middleLayout;
+    QPointer<QLabel> _middleLabel;
+    QPointer<QWidget> _rightColumn;
+    QPointer<QVBoxLayout> _rightLayout;
+    QPointer<QLabel> _rightLabel;
+
 public:
     MainWidget(QWidget *parent = nullptr);
     ~MainWidget();
